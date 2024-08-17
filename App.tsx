1a3833645175621412823a9dff20a9cde77f3804
Max Kaliakin <max.kaliakin@gmail.com>
2024-08-17 20:34:37 +0300

    update nodes

--- App.tsx
+++ App.tsx
@@ -3,8 +3,24 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import AppNavigator from './src/navigation/AppNavigator';
 import {NotifierWrapper} from 'react-native-notifier';
 import './i18n';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
 
 const App = () => {
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  // OneSignal Initialization
+  OneSignal.initialize('e8784bc8-9cd9-41ef-8ca7-f60321aea724');
+
+  // requestPermission will show the native iOS or Android notification permission prompt.
+  // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.Notifications.requestPermission(true);
+
+  // Method for listening for notification clicks
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NotifierWrapper>
